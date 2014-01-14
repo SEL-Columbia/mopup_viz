5c2171eada6f76e30105b268a9cf949714f0f4d4
zmyao88 <zm.yao88@gmail.com>
2014-01-13 23:29:47 -0500

    minor modification and cleaning up before packaging into functions

--- facility_viz.R
+++ facility_viz.R
@@ -10,11 +10,11 @@ require(RgoogleMaps)
 
 # load shapefile and facility list
 wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-nga_shp <- readShapeSpatial("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/raw_data/nga_states/nga_states.shp")
+nga_shp <- readShapeSpatial("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/raw_data/nga_lgas/nga_lgas_with_corrected_id.shp")
 
 # polygon manipulations
-akwa_shp <- subset(nga_shp, Name == "Akwa Ibom")
-akwa_shp_fortify <- fortify(akwa_shp, region="Name")
+current_shp <- subset(nga_shp, lga_id == "2")
+current_shp_fortify <- fortify(current_shp, region="lga_id")
 
 
 
@@ -25,35 +25,14 @@ facilities <- readRDS("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_p
 facilities$lat <- as.numeric(lapply(str_split(facilities$gps, " "), function(x) x[1]))
 facilities$long <- as.numeric(lapply(str_split(facilities$gps, " "), function(x) x[2]))
 
-facilities <- subset(facilities, state == "Akwa Ibom")
+current_facilities <- subset(facilities, lga_id == "2")
 
+#### getting separating grids
+grid_x <- seq(current_shp@bbox['x', 'min'], 
+         current_shp@bbox['x', 'max'], length.out=4)
+grid_y <- seq(current_shp@bbox['y', 'min'],
+         current_shp@bbox['y', 'max'], length.out=4)
 
-# bbox <- c(akwa_shp@bbox['x', 'min'], akwa_shp@bbox['y', 'min'],
-#           akwa_shp@bbox['x', 'max'], akwa_shp@bbox['y', 'max'])
-
-# 
-# my_map <- get_map(location=bbox, maptype = 'satellite')
-# ggmap(my_map) + 
-#   geom_polygon(aes(x=long, y=lat, group=group), data=akwa_shp_fortify,
-#               color = 'red', fill='black', alpha=0.2) +
-#   coord_equal() 
-
-
-
-grid_x <- seq(akwa_shp@bbox['x', 'min'], 
-         akwa_shp@bbox['x', 'max'], length.out=4)
-grid_y <- seq(akwa_shp@bbox['y', 'min'],
-         akwa_shp@bbox['y', 'max'], length.out=4)
-
-# x_center <- get_grid_mean(grid_x)
-# y_center <- get_grid_mean(grid_y)
-
-# text_center <- expand.grid(x=x_center, y=y_center)
-# text_center$word <- 1:nrow(text_center)
-# 
-# get_grid_mean <- function(x){
-#   apply(cbind(x[1:length(x)-1], x[2:length(x)]), 1, mean)
-# }
 
 get_grid_zoomin_bbox <- function(x, y){
   x_coor <- data.frame(x_min = x[1:length(x)-1], 
@@ -70,35 +49,74 @@ get_grid_zoomin_bbox <- function(x, y){
 bbox_data <- get_grid_zoomin_bbox(grid_x, grid_y)
 
 
-lga_name <- "Akwa Ibom"
-# Plotting
-ggplot(akwa_shp_fortify, aes(x=long, y=lat)) +
+lga_name <- current_shp_fortify$id[1]
+# Plotting lga level
+ggplot(current_shp_fortify, aes(x=long, y=lat)) +
   geom_polygon(alpha=0.2) +
   geom_path(color="red") +
-  geom_point(data=facilities, aes(x=long, y=lat), color='black', size=1) + 
+  geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
   geom_vline(xintercept = grid_x) + 
   geom_hline(yintercept = grid_y) +
-  geom_text(data=bbox_df, aes(x=x_center, y=y_center, label=word), size=10, color='blue') + 
+  geom_text(data=bbox_data, aes(x=x_center, y=y_center, label=word), size=10, color='blue') + 
   coord_equal() + 
   theme(panel.grid=element_blank()) + 
-  labs(title = paste('Map of', lga_name,sep=' '))
+  labs(title = paste('Map of lga_id ==', lga_name,sep=' '))
+
+# Plotting area zoom in level
+getting_zoomin_graph <- function(current_bbox_df){
+  plot <- ggplot(current_shp_fortify, aes(x=long, y=lat)) +
+    geom_polygon(alpha=0.2) +
+    geom_path(color="red") +
+    geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
+    geom_vline(xintercept = grid_x) + 
+    geom_hline(yintercept = grid_y) +
+    coord_equal() +
+    theme(panel.grid=element_blank(),
+          panel.background = element_blank()) +
+    coord_map(xlim=c(current_bbox_df$x_min, current_bbox_df$x_max),
+              ylim=c(current_bbox_df$y_min, current_bbox_df$y_max)) +   
+    labs(title = paste('Map of Area', current_bbox_df$word, sep=' '))
+  print(plot)
+}
+
+d_ply(bbox_data, .(word), function(df) getting_zoomin_graph(df))
 
 
-current_bbox_df <- bbox_data[1,]
-# zoom-in view
-ggplot(akwa_shp_fortify, aes(x=long, y=lat)) +
-  geom_polygon(alpha=0.2) +
-  geom_path(color="red") +
-  geom_point(data=facilities, aes(x=long, y=lat), color='black', size=1) + 
-  geom_vline(xintercept = grid_x) + 
-  geom_hline(yintercept = grid_y) +
-  coord_equal() +
-  theme(panel.grid=element_blank()) +
-  scale_x_continuous(limits=c(current_bbox_df$x_min, 
-                              current_bbox_df$x_max)) + 
-  scale_y_continuous(limits=c(current_bbox_df$y_min, 
-                              current_bbox_df$y_max)) + 
-  labs(title = paste('Map of Area', current_bbox_df$word, sep=' '))
 
 
+
+
+# current_bbox_df <- bbox_data[1,]
+# # zoom-in view
+# ggplot(current_shp_fortify, aes(x=long, y=lat)) +
+#   geom_polygon(alpha=0.2) +
+#   geom_path(color="red") +
+#   geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
+#   geom_vline(xintercept = grid_x) + 
+#   geom_hline(yintercept = grid_y) +
+#   coord_equal() +
+#   theme(panel.grid=element_blank(),
+#         panel.background = element_blank()) +
+# 
+#   coord_map(xlim=c(current_bbox_df$x_min, current_bbox_df$x_max),
+#             ylim=c(current_bbox_df$y_min, current_bbox_df$y_max)) +   
+#   labs(title = paste('Map of Area', current_bbox_df$word, sep=' '))
+
+getting_zoomin_graph <- function(current_bbox_df){
+  plot <- ggplot(current_shp_fortify, aes(x=long, y=lat)) +
+          geom_polygon(alpha=0.2) +
+          geom_path(color="red") +
+          geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
+          geom_vline(xintercept = grid_x) + 
+          geom_hline(yintercept = grid_y) +
+          coord_equal() +
+          theme(panel.grid=element_blank(),
+                panel.background = element_blank()) +
+          coord_map(xlim=c(current_bbox_df$x_min, current_bbox_df$x_max),
+                    ylim=c(current_bbox_df$y_min, current_bbox_df$y_max)) +   
+          labs(title = paste('Map of Area', current_bbox_df$word, sep=' '))
+  print(plot)
+}
+
+d_ply(bbox_data, .(word), function(df) getting_zoomin_graph(df))
 
