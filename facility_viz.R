3c2130e84150466fec0d5808df88d85f34f6ec28
zmyao88 <zm.yao88@gmail.com>
2014-01-14 00:37:12 -0500

    lga level done, working on the big loop

--- facility_viz.R
+++ facility_viz.R
@@ -12,28 +12,37 @@ require(RgoogleMaps)
 wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 nga_shp <- readShapeSpatial("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/raw_data/nga_lgas/nga_lgas_with_corrected_id.shp")
 
-# polygon manipulations
-current_shp <- subset(nga_shp, lga_id == "2")
-current_shp_fortify <- fortify(current_shp, region="lga_id")
-
-
-
-
 # facility data processing
 facilities <- readRDS("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Normalized/Education_774_NMIS_Facility.rds")
-
 facilities$lat <- as.numeric(lapply(str_split(facilities$gps, " "), function(x) x[1]))
 facilities$long <- as.numeric(lapply(str_split(facilities$gps, " "), function(x) x[2]))
 
+
+# current lga data subsetting
+current_shp <- subset(nga_shp, lga_id == "2")
+current_shp_fortify <- fortify(current_shp, region="Name")
 current_facilities <- subset(facilities, lga_id == "2")
 
-#### getting separating grids
-grid_x <- seq(current_shp@bbox['x', 'min'], 
-         current_shp@bbox['x', 'max'], length.out=4)
-grid_y <- seq(current_shp@bbox['y', 'min'],
-         current_shp@bbox['y', 'max'], length.out=4)
 
 
+### getting grid line coordinates
+get_grids <- function(current_shp, nrow, ncol){
+  
+  ncol <- ncol + 1
+  nrow <- nrow + 1
+  
+  grid_x <- seq(current_shp@bbox['x', 'min'], 
+                current_shp@bbox['x', 'max'], length.out=ncol)
+  grid_y <- seq(current_shp@bbox['y', 'min'],
+                current_shp@bbox['y', 'max'], length.out=nrow)
+  
+  grid_df <- data.frame(x=grid_x, y=grid_y)
+  return(grid_df)
+}
+
+
+
+#### define function for grid line df 
 get_grid_zoomin_bbox <- function(x, y){
   x_coor <- data.frame(x_min = x[1:length(x)-1], 
                        x_max = x[2:length(x)])
@@ -46,39 +55,62 @@ get_grid_zoomin_bbox <- function(x, y){
   return(bbox_df)
 }
 
-bbox_data <- get_grid_zoomin_bbox(grid_x, grid_y)
+# bbox_data <- get_grid_zoomin_bbox(grid_x, grid_y)
 
-
-lga_name <- current_shp_fortify$id[1]
 
 # Plotting lga level
-ggplot(current_shp_fortify, aes(x=long, y=lat)) +
-  geom_polygon(alpha=0.2) +
-  geom_path(color="red") +
-  geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
-  geom_vline(xintercept = grid_x) + 
-  geom_hline(yintercept = grid_y) +
-  geom_text(data=bbox_data, aes(x=x_center, y=y_center, label=word), size=10, color='blue') + 
-  coord_equal() + 
-  theme(panel.grid=element_blank()) + 
-  labs(title = paste('Map of lga_id ==', lga_name,sep=' '))
-
-# Plotting area zoom in level
-getting_zoomin_graph <- function(current_bbox_df){
+getting_lga_graph <- function(current_shp_fortify, current_facilities,
+                              bbox_data, grid_lines){
+  
+  lga_name <- current_shp_fortify$id[1]
+  
   plot <- ggplot(current_shp_fortify, aes(x=long, y=lat)) +
-    geom_polygon(alpha=0.2) +
-    geom_path(color="red") +
-    geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
-    geom_vline(xintercept = grid_x) + 
-    geom_hline(yintercept = grid_y) +
-    coord_equal() +
-    theme(panel.grid=element_blank(),
-          panel.background = element_blank()) +
-    coord_map(xlim=c(current_bbox_df$x_min, current_bbox_df$x_max),
-              ylim=c(current_bbox_df$y_min, current_bbox_df$y_max)) +   
-    labs(title = paste('Map of Area', current_bbox_df$word, sep=' '))
+            geom_polygon(alpha=0.2) +
+            geom_path(color="red") +
+            geom_point(data=current_facilities, 
+                       aes(x=long, y=lat), color='black', size=1) + 
+            geom_vline(xintercept = grid_lines$x) + 
+            geom_hline(yintercept = grid_lines$y) +
+            geom_text(data=bbox_data, 
+                      aes(x=x_center, y=y_center, label=word),
+                      size=10, color='blue') + 
+            coord_equal() + 
+            theme(panel.grid=element_blank()) + 
+            labs(title = paste('Map of', lga_name,sep=' '))
   print(plot)
 }
 
-d_ply(bbox_data, .(word), function(df) getting_zoomin_graph(df))
+# Plotting area zoom in level
+getting_zoomin_graph <- function(current_bbox_df,
+                                 current_shp_fortify, current_facilities, grid_lines){
+  
+  plot <- ggplot(current_shp_fortify, aes(x=long, y=lat)) +
+            geom_polygon(alpha=0.2) +
+            geom_path(color="red") +
+            geom_point(data=current_facilities, aes(x=long, y=lat), color='black', size=1) + 
+            geom_vline(xintercept = grid_lines$x) + 
+            geom_hline(yintercept = grid_lines$y) +
+            coord_equal() +
+            theme(panel.grid=element_blank(),
+                  panel.background = element_blank()) +
+            coord_map(xlim=c(current_bbox_df$x_min, current_bbox_df$x_max),
+                      ylim=c(current_bbox_df$y_min, current_bbox_df$y_max)) +   
+            labs(title = paste('Map of Area', current_bbox_df$word, sep=' '))
+  print(plot)
+}
 
+# Creating master function to plot lga overview + zoomin level all at once
+lga_viz <- function(current_shp_fortify, current_facilities){
+  
+  grid_lines <- get_grids(current_shp, 3, 3)
+  bbox_data <- get_grid_zoomin_bbox(grid_lines$x, grid_lines$y)
+  
+  getting_lga_graph(current_shp_fortify, 
+                    current_facilities, bbox_data, grid_lines)
+  
+  d_ply(bbox_data, .(word), function(df) getting_zoomin_graph(df, current_shp_fortify, 
+                                          current_facilities, grid_lines))
+  
+}
+
+lga_viz(current_shp_fortify, current_facilities)
